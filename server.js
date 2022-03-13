745a3d14af2850f2bd9fe2ff0758cc729e1fc8aa
John Andrew Moquia <andrewmoquia09@gmail.com>
2022-03-13 19:03:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,16 +20,32 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function (req, res) {
+  const date = Date.now()
+  res.json({unix: date, utc: new Date(date).toGMTString()})
 });
 
 app.get("/api/:date", (req, res) => {
   const { date } = req.params;
-  const formatDate = new Date(Number(date)).toGMTString();
-  formatDate === "Invalid Date"
-  ? res.json({error: "Invalid Date"})
-  : res.json({unix: Number(date), utc: formatDate });
+
+  const regex = new RegExp('-');
+
+  if(regex.test(date)) {
+    
+    const formatDate = new Date(date);
+    const dateInGMT = new Date(formatDate).toGMTString();
+    dateInGMT === "Invalid Date"
+    ? res.json({error: "Invalid Date"})
+    : res.json({unix: new Date(date).valueOf(), utc: dateInGMT });
+    
+  } else {
+    
+    const dateInGMT = new Date(Number(date)).toGMTString();
+    dateInGMT === "Invalid Date"
+    ? res.json({error: "Invalid Date"})
+    : res.json({unix: Number(date), utc: dateInGMT });
+    
+  }
 });
 
 // listen for requests :)
